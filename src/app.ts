d432a1a5722ac97a2cb11f053aa3906c0ce345f2
Guilherme Rocha <guilhermerochapsilva@gmail.com>
2022-03-30 09:38:54 -0300

    fixed redis connection

--- src/app.ts
+++ src/app.ts
@@ -1,44 +1,46 @@
 import express from 'express';
 
 import { generate } from 'short-uuid';
-import { createClient } from 'redis';
-
+import { createClient, RedisClientType } from 'redis';
 
 const app = express();
 
 app.use(express.json());
 
-
+let client: RedisClientType;
 (async () => {
-    const client = createClient();
+    client = createClient();
 
-    client.on('error', (err) => console.log("Redis client error: ", err))
+    client.on('error', (err) => console.log("Redis client error: ", err));
 
     await client.connect();
 
-
-    app.post('/create_shortened_url', async (request, response) => {
-        const {originalURL} = request.body;
-    
-        const shortnedURL = `${generate()}`;
-
-        await client.set(shortnedURL, originalURL);
-    
-        return response.status(201).json({shortnedURL});
-    });
-    
-    app.get("/:URL", async (request, response) => {
-        const {URL} = request.params;
-    
-        const originalURL = await client.get(URL);
-    
-        if(!originalURL) {
-            response.status(404).json({message: "Not found"});
-    
-        } else {
-            response.redirect(originalURL);
-        }
-    });
+    return client;
 })();
+
+
+app.post('/create_shortened_url', async (request, response) => {
+    const {originalURL} = request.body;
+
+    const shortnedURL = `${generate()}`;
+
+    await client.set(shortnedURL, originalURL);
+
+    return response.status(201).json({shortnedURL});
+});
+
+app.get("/:URL", async (request, response) => {
+    const {URL} = request.params;
+
+    const originalURL = await client.get(URL);
+
+    if(!originalURL) {
+        response.status(404).json({message: "Not found"});
+
+    } else {
+        response.redirect(originalURL);
+    }
+});
+
 
 export default app;
